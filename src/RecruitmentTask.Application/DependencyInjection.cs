60622efb75c77a97608a85b40787f408e59604af
Sebastian Szafran <vbdeveloping@outlook.com>
2024-05-09 21:26:01 +0200

    Minor changes in application layer

--- src/RecruitmentTask.Application/DependencyInjection.cs
+++ src/RecruitmentTask.Application/DependencyInjection.cs
@@ -8,15 +8,19 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        var thisAssembly = typeof(DependencyInjection).Assembly;
+
         services.AddMediatR(configuration =>
         {
-            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            configuration.RegisterServicesFromAssembly(thisAssembly);
 
             configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
             configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
 
-        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+        services.AddValidatorsFromAssembly(thisAssembly);
+
+        services.AddAutoMapper(thisAssembly);
 
         return services;
     }
