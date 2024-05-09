04845153d662ba71baadedd7805c7887e7eccfd5
Sebastian Szafran <vbdeveloping@outlook.com>
2024-05-09 21:25:02 +0200

    Implemented getting person by id query

---
+++ src/RecruitmentTask.Application/People/GetPerson/GetPersonQuery.cs
@@ -0,0 +1,7 @@
+﻿using RecruitmentTask.Application.Abstraction.Messaging;
+using RecruitmentTask.Application.People.GetAllPeople;
+using System;
+
+namespace RecruitmentTask.Application.People.GetPerson;
+
+public sealed record GetPersonQuery(Guid Id) : IQuery<PersonResponse>;
---
+++ src/RecruitmentTask.Application/People/GetPerson/GetPersonQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using RecruitmentTask.Application.Abstraction.Messaging;
+using RecruitmentTask.Application.People.GetAllPeople;
+using RecruitmentTask.Domain.Abstractions;
+using RecruitmentTask.Domain.People;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RecruitmentTask.Application.People.GetPerson;
+
+internal sealed class GetPersonQueryHandler : IQueryHandler<GetPersonQuery, PersonResponse>
+{
+    private readonly IPersonRepository _personRepository;
+    private readonly IMapper _mapper;
+
+    public GetPersonQueryHandler(
+        IPersonRepository personRepository,
+        IMapper mapper)
+    {
+        _personRepository = personRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<PersonResponse>> Handle(GetPersonQuery request, CancellationToken cancellationToken)
+    {
+        var person = await _personRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (person is null)
+        {
+            return Result.Failure<PersonResponse>(PersonErrors.NotFound);
+        }
+
+        var personResponse = _mapper.Map<PersonResponse>(person);
+
+        return personResponse;
+    }
+}
