0cfde997c87988971ed85fedb522f53f52e92afe
Sebastian Szafran <vbdeveloping@outlook.com>
2024-05-12 03:13:54 +0200

    Restructured solution files & added architectional tests project

---
+++ test/RecruitmentTask.ArchitectureTests/Domain/DomainTests.cs
@@ -0,0 +1,35 @@
+﻿using NetArchTest.Rules;
+using RecruitmentTask.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace RecruitmentTask.ArchitectureTests.Domain;
+
+public class DomainTests : BaseTest
+{
+    [Fact]
+    public void Entities_ShouldHave_PrivateParameterlessConstructor()
+    {
+        var entityTypes = Types.InAssembly(DomainAssembly)
+            .That()
+            .Inherit(typeof(Entity))
+            .GetTypes();
+
+        var failingTypes = new List<Type>();
+
+        foreach (var entityType in entityTypes)
+        {
+            var constructors = entityType.GetConstructors(BindingFlags.NonPublic |
+                                                          BindingFlags.Instance);
+
+            if (!constructors.Any(c => c.IsPrivate && c.GetParameters().Length == 0))
+            {
+                failingTypes.Add(entityType);
+            }
+        }
+
+        Assert.Empty(failingTypes);
+    }
+}
---
+++ test/RecruitmentTask.ArchitectureTests/Layers/LayerTests.cs
@@ -0,0 +1,61 @@
+﻿using NetArchTest.Rules;
+
+namespace RecruitmentTask.ArchitectureTests.Layers;
+
+public class LayerTests : BaseTest
+{
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOn_ApplicationLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void InfrastructureLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+}
